0421c285ca485ca1f82c0f1355fe5a515642d45c
julianiiriani <julianiikalangie@gmail.com>
2025-11-03 02:49:55 +0800

    Add Tailwind CSS and setup components folder

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -7,12 +6,3 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 06458df261e8ce708977e91e4dab9efb93c35c0a
